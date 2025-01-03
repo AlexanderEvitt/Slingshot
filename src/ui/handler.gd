812ac10c8ede9f18fba817905589b9ee3c79eb22
AlexanderEvitt <alexander.evitt@gmail.com>
2025-01-02 21:15:51 -0500

    Arrow keys for HUD switch

--- src/ui/handler.gd
+++ src/ui/handler.gd
@@ -2,6 +2,7 @@ extends VBoxContainer
 
 var huds
 var butts
+var h = 0
 
 
 # Get refs to all the HUDs and buttons
@@ -9,21 +10,35 @@ func _ready():
 	huds = get_tree().get_nodes_in_group("HUDs")
 	butts = get_tree().get_nodes_in_group("Buttons")
 
+func _input(_event):
+	# Handle arrow keys switching HUD
+	if Input.is_action_just_pressed("arrow_up"):
+		h = h - 4
+	elif Input.is_action_just_pressed("arrow_down"):
+		h = h + 4
+	elif Input.is_action_just_pressed("arrow_left"):
+		h = h - 1
+	elif Input.is_action_just_pressed("arrow_right"):
+		h = h + 1
+	h = clamp(h,0,7)
+	set_huds(h)
 
 func set_huds(i):
+	h = i # used for keeping track of which HUD is active
+	
 	# Set all the buttons to be un-toggled except the one just pressed
 	for b in butts:
 		if butts.find(b,0) == i:
-			pass
+			b.set_pressed_no_signal(true)
 		else:
-			b.button_pressed = false
+			b.set_pressed_no_signal(false)
 	
 	# Set the correct HUD to be visible
-	for h in huds:
-		if huds.find(h,0) == i:
-			h.visible = true
+	for hud in huds:
+		if huds.find(hud,0) == i:
+			hud.visible = true
 		else:
-			h.visible = false
+			hud.visible = false
 
 
 func _on_button1_pressed():
