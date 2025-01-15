9157d3f23b69774f0603639110831a14a13cde5f
AlexanderEvitt <50218791+AlexanderEvitt@users.noreply.github.com>
2025-01-14 23:13:51 -0500

    Prototype ship

--- src/visuals/plume_controller.gd
+++ src/visuals/plume_controller.gd
@@ -1,15 +1,18 @@
 extends Node3D
 
+var chamber
+var tail
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	tail = get_node("Tail")
+	chamber = get_node("ThrustChamber")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if OwnShip.thrust > 0:
 		visible = true
-		scale.x = 3*OwnShip.thrust/0.01
+		tail.scale.x = OwnShip.thrust/0.05
 	else:
 		visible = false
