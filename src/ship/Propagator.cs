840a6879327d85b5a6cac5786115a1fb020fc3fe
AlexanderEvitt <50218791+AlexanderEvitt@users.noreply.github.com>
2025-02-25 11:27:39 -0500

    Frigate
    New ship model

--- src/ship/Propagator.cs
+++ src/ship/Propagator.cs
@@ -159,11 +159,11 @@ public partial class Propagator : Node3D
 			Vector3 a = Acceleration(planned_positions[i],planned_times[i]);
 
 			// Calculate velocity normal to trajectory and counter it
-			//planned_velocities[i];
+			Vector3 correction = planned_velocities[i];
 
 			// Calculate control as antigravity + in direction or opposite if more than halfway
 			Vector3 flight_direction = target_position - planned_positions[i];
-			planned_controls.Add(flip*0.01d*flight_direction.Normalized() - a);
+			planned_controls.Add(flip*0.01d*flight_direction.Normalized() - a + correction ); asdf
 			
 			// Propagate to next timestep
 			(Vector3 np,Vector3 nv,double nt) = StepVerlet(planned_positions[i],planned_velocities[i],planned_controls[i],planned_times[i]);
